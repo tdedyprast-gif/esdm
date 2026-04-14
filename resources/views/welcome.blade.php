03a1a49959b7904c28ef2e8c4a9aeae849d21ac4
assquqoniyusril <assauqoni.yusril@icloud.com>
2026-04-15 06:39:33 +0700

    Ganti link bootstraps 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <title>ESDM - STKIP PGRI Pacitan</title>
 
     <!-- Bootstrap 5 CSS -->
-    <link href="https://jsdelivr.net" rel="stylesheet">
+    <link href="<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">" rel="stylesheet">
 
     <!-- Google Fonts Poppins (Link Aktif) -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -124,7 +124,7 @@
     <script src="https://jquery.com"></script>
 
     <!-- 2. Panggil Bootstrap Bundle JS (Sudah termasuk Popper) -->
-    <script src="https://jsdelivr.net"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <!-- 3. Skrip Custom Anda -->
     <script>
